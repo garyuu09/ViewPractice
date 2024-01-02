497a5658a0e5886a3f120506652d83d4e33050f9
Ryuga <ribereo.minami@gmail.com>
2024-01-02 10:57:10 +0900

    ViewBuiderの仕様を確認した。

--- ViewPractice/ContentView.swift
+++ ViewPractice/ContentView.swift
@@ -15,7 +15,27 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
-        .padding()
+
+        InsideMultiView()
+        OutSideMultiView()
+    }
+///    ViewBuilderがないと以下のエラーが出る。
+///    Function declares an opaque return type, but has no return statements in its body from which to infer an underlying type
+    @ViewBuilder
+    private func InsideMultiView() -> some View {
+        Image(systemName: "plus")
+        Image(systemName: "plus")
+        Image(systemName: "plus")
+    }
+}
+
+struct OutSideMultiView: View {
+    // bodyプロパティに暗黙的にViewBuilderが使われているので、
+    // 複数のViewを記載することができる。
+    var body: some View {
+        Image(systemName: "plus.circle")
+        Image(systemName: "plus.circle")
+        Image(systemName: "plus.circle")
     }
 }
 
