4552ee81a83cedc81e07155a769d794553f5c704
Ryuga <ribereo.minami@gmail.com>
2024-01-02 09:58:04 +0900

    Initial Commit

---
+++ ViewPractice/ViewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewPracticeApp.swift
+//  ViewPractice
+//
+//  Created by Ryuga on 2024/01/02.
+//
+
+import SwiftUI
+
+@main
+struct ViewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
